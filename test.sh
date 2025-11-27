661dc78808dd43aee2dbc721d36b81ffff8c9d5e
JThuge <45138940+JThuge@users.noreply.github.com>
2025-11-27 19:44:54 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@ CUDA_VISIBLE_DEVICES=0 \
 python cirr_test_submission.py \
    --save-name slot_light \
    --blip-model-name blip2_light_cir \
+   --config-path path to the .json config file \
    --model-path path to the .pt checkpoint \
